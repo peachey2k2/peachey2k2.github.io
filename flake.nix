f00461b548a8339ab7f3867a25e2a557b95f4974
peachey2k2 <pe@chey>
2025-11-30 10:36:45 +0300

    overhaul the about page, switch the font and fix mobile quirks

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
   };
 
   outputs = { self, nixpkgs }:
@@ -30,7 +30,7 @@
 
         cp "favicon.ico" "$out/favicon.ico"
         cp "style.css" "$out/style.css"
-        cp "VT323.ttf" "$out/VT323.ttf"
+        cp "Miracode.ttf" "$out/Miracode.ttf"
         cp -r "88x31" "$out/88x31"
         cp -r "images" "$out/images"
 
@@ -40,20 +40,20 @@
           botLine="$(echo "$splittedTitle" | tail -n +2)"
 
           asp=$(${pkgs.imagemagick}/bin/identify -format '%[fx:w/h]' "images/$blogImage")
-          
+
           ${pkgs.imagemagick}/bin/magick -size 1200x800 xc:#0000ff \
-          -font VT323.ttf -gravity Center -pointsize 72 \
-          -fill \#000000 -draw 'rectangle 160, 100 1120, 740' \
-          -fill \#b2b2b2 -draw 'rectangle 120, 60 1080, 700' \
-          -fill \#000000 -annotate +0+180 "$topLine" \
-          -fill \#000000 -annotate +0+250 "$botLine" \
-          -fill \#000000 -draw "rectangle %[fx:600-210*$asp-20], 80 %[fx:600+210*$asp+20], 540" \
-          -fill \#b2b2b2 -draw "rectangle %[fx:600-210*$asp-16], 84 %[fx:600+210*$asp+16], 536" \
-          -fill \#b2b2b2 -draw 'rectangle 500, 78 700, 86' \
-          -pointsize 40 \
-          -fill \#000000 -annotate +0-320 "$blogDate" \
-          -draw "image SrcOver 0, -90 %[fx:420*$asp], 420 images/$blogImage" \
-          "$out/images/thumb_$blogImage"
+            -font Miracode.ttf -gravity Center -pointsize 72 \
+            -fill \#000000 -draw 'rectangle 160, 100 1120, 740' \
+            -fill \#b2b2b2 -draw 'rectangle 120, 60 1080, 700' \
+            -fill \#000000 -annotate +0+180 "$topLine" \
+            -fill \#000000 -annotate +0+250 "$botLine" \
+            -fill \#000000 -draw "rectangle %[fx:600-210*$asp-20], 80 %[fx:600+210*$asp+20], 540" \
+            -fill \#b2b2b2 -draw "rectangle %[fx:600-210*$asp-16], 84 %[fx:600+210*$asp+16], 536" \
+            -fill \#b2b2b2 -draw 'rectangle 500, 78 700, 86' \
+            -pointsize 40 \
+            -fill \#000000 -annotate +0-320 "$blogDate" \
+            -draw "image SrcOver 0, -90 %[fx:420*$asp], 420 images/$blogImage" \
+            "$out/images/thumb_$blogImage"
         }
 
         buildMainPage() {
@@ -62,34 +62,34 @@
             sed 's/@desc@//g' |
             sed 's/@image@//g' |
             sed 's/@url@//g'                  >> "$out/index.html"
-          echo '<div id="markdown-content">'  >> "$out/index.html"
-          cat "markdown/about.md" |
-            ${marked}/bin/marked -c "marked.js" |
-            ${hljs}/bin/hljs                  >> "$out/index.html"
-          echo '</div>'                       >> "$out/index.html"
+          # echo '<div id="markdown-content">'  >> "$out/index.html"
+          cat "about.html" |
+            # ${marked}/bin/marked -c "marked.js" |
+            ${hljs}/bin/hljs --s=".code-hl"   >> "$out/index.html"
+          # echo '</div>'                       >> "$out/index.html"
           cat "template/tail.html"            >> "$out/index.html"
         }
-        
+
         buildBlogPost() {
           mkdir -p "$out/b/$1"
           cat "template/head.html" |
             sed "s/@title@/$blogTitle - peachey2k2/g" |
             sed "s/@desc@/$blogTldr/g" |
             sed "s/@image@/\/images\/thumb_$blogImage/g" |
-            sed "s/@url@/\/b\/$1/g"            >> "$out/b/$1/index.html"
+            sed "s/@url@/\/b\/$1/g"             >> "$out/b/$1/index.html"
 
-          echo '<div id="markdown-content">'   >> "$out/b/$1/index.html"
+          echo '<div id="markdown-content">'    >> "$out/b/$1/index.html"
           echo "![$blogDate](/images/$blogImage)" |
             ${marked}/bin/marked -c "marked.js" >> "$out/b/$1/index.html"
           echo "<h1 class=centered style=\"text-decoration: underline\">$blogTitle</h1> <p>" >> "$out/b/$1/index.html"
           echo "TL;DR - $blogTldr" | ${marked}/bin/marked >> "$out/b/$1/index.html"
-          echo '</p> </div>'                        >> "$out/b/$1/index.html"
+          echo '</p> </div>'                    >> "$out/b/$1/index.html"
 
-          echo '<div id="markdown-content">'   >> "$out/b/$1/index.html"
+          echo '<div id="markdown-content">'    >> "$out/b/$1/index.html"
           ${marked}/bin/marked -c "marked.js" |
-            ${hljs}/bin/hljs                   >> "$out/b/$1/index.html"
-          echo '</div>'                        >> "$out/b/$1/index.html"
-          cat "template/tail.html"             >> "$out/b/$1/index.html"
+            ${hljs}/bin/hljs --s=".code-hl"     >> "$out/b/$1/index.html"
+          echo '</div>'                         >> "$out/b/$1/index.html"
+          cat "template/tail.html"              >> "$out/b/$1/index.html"
         }
 
         buildBlogsList() {
@@ -98,9 +98,9 @@
             sed "s/@title@/blogs - peachey2k2/g" |
             sed "s/@desc@//g" |
             sed "s/@image@//g" |
-            sed "s/@url@/\/blog/g"             >> "$out/blog/index.html"
-          echo "<div class='blog-list'>"       >> "$out/blog/index.html"
-          
+            sed "s/@url@/\/blog/g"        >> "$out/blog/index.html"
+          echo "<div class='blog-list'>"  >> "$out/blog/index.html"
+
           mkdir -p "$out/b"
           touch "$out/blog-list.ini"
 
@@ -113,7 +113,7 @@
                 if [[ "$line" == "---" ]]; then
 
                   createThumbnail
-                  
+
                   echo "
                     <div class="blog-card">
                       <div class="blog-image">
@@ -129,7 +129,8 @@
                       </h3>
                     </div>
                   " >> "$out/blog/index.html"
-                  buildBlogPost "$nameHash"                  break
+                  buildBlogPost "$nameHash"
+                  break
                 fi
 
                 curKey="$(echo "$line" | cut -d '=' -f 1 | xargs)"
@@ -171,7 +172,7 @@
         '';
       in
         "${script}/bin/run-server";
-        
+
     };
 
     defaultPackage.x86_64-linux = self.packages.default;
--- marked.js
+++ marked.js
@@ -12,6 +12,7 @@ module.exports = {
         </div>
       `;
     },
+
     heading({depth, text}) {
       const decors = [
         "#######",
@@ -25,18 +26,26 @@ module.exports = {
         </h${depth}>
       `;
     },
-    code({lang, text}) {
-      const escapedText = escape(text);
+
+    code({lang: display, text: code}) {
+      const escapedText = escape(code);
+      const parts = display.split('@')
 
       return `
         <div class="centered-div">
           <div class="blog-image" style="display: inline-flex">
-            <pre style="margin: 8px; overflow-y: auto"><code class="language-${lang}">${escapedText}</code></pre>
+            <pre style="margin: 8px; overflow-y: auto"><code class="hljs ${(parts[0] ? "code-hl language-" + parts[0] : "")}">${escapedText}</code></pre>
             <div class="blog-image-text">
-              ${lang}
+              ${parts[1] || ""}
             </div>
           </div>
         </div>
+      `;
+    },
+
+    codespan({text}) {
+      return `
+        <code class="codespan">${text}</code>
       `;
     }
   },
